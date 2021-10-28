dd071ce1a313dce9a12dd2930254852892442179
Даниэла Павлова <linaquinzel@MacBook-Air-Daniela.local>
2021-10-28 15:33:56 +0400

    now

--- test_triangles.py
+++ test_triangles.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-img = cv2.imread('triangles_and_squares.jpg')  # read image from system
+img = cv2.imread('test_images/test22.jpg')  # read image from system
 list1 = []
 list2 = []
-lower = np.array([76, 0, 153], dtype="uint8")
-upper = np.array([255, 204, 255], dtype="uint8")
+lower = np.array([0, 0, 150], dtype="uint8")
+upper = np.array([102, 102, 255], dtype="uint8")
 mask = cv2.inRange(img, lower, upper)
     # блюрим изображение (картинка, (размер ядра(матрицы), стандартное отклонение ядра))
 blurred = cv2.GaussianBlur(mask, (3, 3), 0)
@@ -13,7 +13,7 @@ blurred = cv2.GaussianBlur(mask, (3, 3), 0)
 T, returned_image = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
 cv2.imshow('matches', img)
 cv2.waitKey(0)
-cv2.imshow('matches', returned_image)
+cv2.imshow('ret', returned_image)
 cv2.waitKey(0)
 contours, hierarchy = cv2.findContours(returned_image, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
 
@@ -26,23 +26,8 @@ def detectShape(c):  # Function to determine type of polygon on basis of number 
         shape = 'on'
         list2.append(1)
     elif(sides == 4):
-        x, y, w, h = cv2.boundingRect(cnt)
-        aspectratio = float(w)/h
-        if (aspectratio == 1):
-            shape = 'square'
-        else:
-            shape = "off"
-            list2.append(0)
-    elif(sides == 5):
-        shape = 'pentagon'
-    elif(sides == 6):
-        shape = 'hexagon'
-    elif(sides == 8):
-        shape = 'octagon'
-    elif(sides == 10):
-        shape = 'star'
-    else:
-        shape = 'circle'
+        shape = 'off'
+        list2.append(0)
     return shape
 
 
@@ -53,17 +38,14 @@ for cnt in contours:
     cx = int(moment['m10'] / moment['m00'])
     cy = int(moment['m01'] / moment['m00'])
     shape = detectShape(cnt)
-    print(list2)
-    
     cv2.drawContours(img, [cnt], -1, (0, 255, 0), 2)
     cv2.putText(img, shape, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0,0),2)  #Putting name of polygon along with the shape 
     cv2.imshow('polygons_detected', img)
-    
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 for cn in range(0, len(contours)):
         x, y, w, h = cv2.boundingRect(contours[cn])
-        if 10 < w and 10 < h:
+        if 1 < w and 1 < h:
             u = (x, y, list2[cn])
             list1.append(u)
             for i in range(len(list1)-1):
@@ -73,4 +55,5 @@ for cn in range(0, len(contours)):
                     elif list1[j][1] == list1[j+1][1]:
                         if list1[j][0] > list1[j+1][0]:
                             list1[j], list1[j+1] = list1[j+1], list1[j]
-print(list1)
+for kl in list1:
+    print(kl[2])
